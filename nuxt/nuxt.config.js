14fb7f6c67a03dda171239f40d0df14e6b29b8bc
monsidev <monsidev@gmail.com>
2020-12-28 19:02:27 +0300

    BASE URL

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -41,53 +41,46 @@
 // }
 export default {
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "server",
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'lk.leadz.monster',
+    title: "lk.leadz.monster",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   router: {
-    middleware: [
-      'clearValidationErrors',
-    ],
+    middleware: ["clearValidationErrors"]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    'ant-design-vue/dist/antd.css',
-    '~/assets/sass/app.scss'
-  ],
+  css: ["ant-design-vue/dist/antd.css", "~/assets/sass/app.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@/plugins/antd-ui',
-    '@/plugins/axios',
-    '@/plugins/v-mask',
-    '@/plugins/bitrix24',
+    "@/plugins/antd-ui",
+    "@/plugins/axios",
+    "@/plugins/v-mask",
+    "@/plugins/bitrix24",
 
-    '@/plugins/mixins/validation',
-    '@/plugins/mixins/demoMixin',
+    "@/plugins/mixins/validation",
+    "@/plugins/mixins/demoMixin",
 
     // {
     //   src: '@/plugins/mojs',
     //   mode: 'client'
     // },
     {
-      src: './plugins/VueAudio',
-      mode: 'client',
-    },
+      src: "./plugins/VueAudio",
+      mode: "client"
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -96,14 +89,11 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build"
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth"],
 
   /**
    * Auth module configuration
@@ -116,36 +106,36 @@ export default {
       local: {
         endpoints: {
           login: {
-            url: '/authorize',
-            method: 'post',
-            propertyName: 'token',
+            url: "/authorize",
+            method: "post",
+            propertyName: "token"
           },
           user: {
-            url: '/user/me',
-            method: 'get',
-            propertyName: 'data'
+            url: "/user/me",
+            method: "get",
+            propertyName: "data"
           },
           logout: {
-            url: '/logout',
-            method: 'get',
+            url: "/logout",
+            method: "get"
           }
         }
-      },
+      }
     },
     redirect: {
-      login: '/sign',
-      logout: '/sign',
-      home: '/',
+      login: "/sign",
+      logout: "/sign",
+      home: "/"
     },
     plugins: [
-      '@/plugins/auth',
-      '@/plugins/directory',
-      '@/plugins/user',
+      "@/plugins/auth",
+      "@/plugins/directory",
+      "@/plugins/user",
 
-      '@/plugins/mixins/directoryMixin',
-      '@/plugins/mixins/userMixin',
-      '@/plugins/mixins/tinkoffMixin',
-      '@/plugins/mixins/paymentMixin',
+      "@/plugins/mixins/directoryMixin",
+      "@/plugins/mixins/userMixin",
+      "@/plugins/mixins/tinkoffMixin",
+      "@/plugins/mixins/paymentMixin"
     ]
   },
 
@@ -154,11 +144,11 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: process.env.NODE_BASE_URL,
+    baseURL: "https://lk.leadz.monster/api"
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    transpile: ['@nuxtjs/auth']
+    transpile: ["@nuxtjs/auth"]
   }
-}
+};
