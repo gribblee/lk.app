5d865ddad255208bed6d1de711281b0b262f36b5
monsidev <monsidev@gmail.com>
2021-01-18 17:31:41 +0300

    Upd Regions Head Title Task #1037

--- nuxt/pages/setting/admin/regions.vue
+++ nuxt/pages/setting/admin/regions.vue
@@ -6,7 +6,10 @@
       @back="() => $router.go(-1)"
     ></a-page-header>
     <a-layout class="setting-layout-sider" ref="siderSetting">
-      <b-layout-sider menu-key="setting-admin-regions" :open-keys="['setting-admin-directory']" />
+      <b-layout-sider
+        menu-key="setting-admin-regions"
+        :open-keys="['setting-admin-directory']"
+      />
       <a-layout-content class="setting-layout-content">
         <a-row :gutter="[16, 16]" type="flex" justify="space-around">
           <a-col :xs="22" :md="10" :lg="10"> </a-col>
@@ -19,6 +22,11 @@
 import Vue from "vue";
 export default Vue.extend({
   name: "setting-admin-regions",
+  head() {
+    return {
+      title: "Администрирование регионы",
+    };
+  },
   data() {
     return {};
   },
