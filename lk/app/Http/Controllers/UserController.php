e9dbeaf040b6fbbd4bd109dc47b054512ff973bc
monsidev <monsidev@gmail.com>
2021-02-12 16:35:57 +0300

    Date Format Auth

--- lk/app/Http/Controllers/UserController.php
+++ lk/app/Http/Controllers/UserController.php
@@ -285,7 +285,7 @@ class UserController extends Controller
         $Response->deals_count = Deal::getCount($request->user()->role);
         $Response->type_name = Category::find($request->user()->category_id)->name;
         $Response->notifications = Notification::nonView($request->user()->id)->get();
-        User::find($request->user()->id)->update([
+        User::where('id', $request->user()->id)->update([
             'was_online' => Carbon::now()->format("d-m-Y H:i:s")
         ]);
         Notification::updatedAllView($request->user()->id, true);
