a0032e93c27b34ce076587125e209f2ceda2ba97
monsidev <monsidev@gmail.com>
2021-02-26 14:44:09 +0300

    Tasks #11111

--- lk/app/Http/Controllers/RunOnController.php
+++ lk/app/Http/Controllers/RunOnController.php
@@ -69,7 +69,7 @@ class RunOnController extends Controller
                 Log::info("Claims is not Empty");
                 $claim = $this->RunOn->claimsMatch($claims, $region); //Поиск совпадения
                 if ($claim) { //Вторая проверка
-                    Log::info("Claim Founed");
+                    Log::info("Claim Founed ID {$claim->id} Consumption {$claim->consumption}");
                     $user = User::where(['role' => 'ROLE_USER', 'is_delete' => false])->find($claim->user_id);
                     if ($user) { //Заявка не должна попадать менеджеру или администратору
                         //** Обновление поступившей заявки
