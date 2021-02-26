f1012f934108511b43df4ed3956f9ac5ed29f0b4
monsidev <monsidev@gmail.com>
2021-02-26 14:31:00 +0300

    Tasks #11111

--- lk/app/Helpers/RunOn.php
+++ lk/app/Helpers/RunOn.php
@@ -16,7 +16,7 @@ class RunOn
     {
     }
 
-    public function claimsMatch($claims, $region)
+    public function claimsMatch(SuperCollection $claims, $region)
     {
         $userKeyed = $claims->mapWithKeys(function ($item) {
             return [$item['user_id']];
