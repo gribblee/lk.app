2f3fd34a1f97186796e947167654910ac66493c1
monsidev <monsidev@gmail.com>
2021-03-14 23:38:48 +0300

    dsad

--- lk/resources/views/web/companies.blade.php
+++ lk/resources/views/web/companies.blade.php
@@ -31,7 +31,7 @@
             return el;
         },
         init: function () {
-            this.data = JSON.parse('{!!$companies!!}');
+            this.data = {!!$companies!!};
             let el = document.getElementById("id-companies-rating");
             if (typeof el != "undefined") {
                 if (el != null) {
