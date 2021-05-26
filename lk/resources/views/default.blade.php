0fab77ddfec62a36338a5bda48dd1caef9560d9a
monsidev <monsidev@gmail.com>
2021-05-26 13:25:30 +0300

    dasda

--- lk/resources/views/default.blade.php
+++ lk/resources/views/default.blade.php
@@ -2,130 +2,223 @@
 <html lang="en" data-n-head="%7B%22lang%22:%7B%221%22:%22en%22%7D%7D">
 
 <head>
-    <title>front.lk</title>
+    <title>Главная</title>
     <meta data-n-head="1" charset="utf-8">
     <meta data-n-head="1" name="viewport" content="width=device-width,initial-scale=1">
     <meta data-n-head="1" data-hid="description" name="description" content="">
     <link data-n-head="1" rel="icon" type="image/x-icon" href="/favicon.ico">
-    <link rel="preload" href="/_nuxt/0b5f6f3.js" as="script">
-    <link rel="preload" href="/_nuxt/3d380bf.js" as="script">
-    <link rel="preload" href="/_nuxt/acd3085.js" as="script">
-    <link rel="preload" href="/_nuxt/4f0ec5f.js" as="script">
+    <link rel="preload" href="/_nuxt/f52530e.js" as="script">
+    <link rel="preload" href="/_nuxt/47db05d.js" as="script">
+    <link rel="preload" href="/_nuxt/af631b6.js" as="script">
+    <link rel="preload" href="/_nuxt/58c456c.js" as="script">
 </head>
 
 <body>
     <div id="__nuxt">
         <style>
-            #nuxt-loading {
+            #__nuxt,
+            body,
+            html {
                 background: #fff;
-                visibility: hidden;
-                opacity: 0;
-                position: absolute;
-                left: 0;
-                right: 0;
-                top: 0;
-                bottom: 0;
+                width: 100%;
+                height: 100%;
                 display: flex;
                 justify-content: center;
                 align-items: center;
-                flex-direction: column;
-                animation: nuxtLoadingIn 10s ease;
-                -webkit-animation: nuxtLoadingIn 10s ease;
-                animation-fill-mode: forwards;
-                overflow: hidden
+                margin: 0;
+                padding: 0
             }
 
-            @keyframes nuxtLoadingIn {
-                0% {
-                    visibility: hidden;
-                    opacity: 0
+            .sk-circle {
+                width: 40px;
+                height: 40px;
+                position: relative
+            }
+
+            .sk-circle .sk-child {
+                width: 100%;
+                height: 100%;
+                position: absolute;
+                left: 0;
+                top: 0
+            }
+
+            .sk-circle .sk-child:before {
+                content: '';
+                display: block;
+                margin: 0 auto;
+                width: 15%;
+                height: 15%;
+                background-color: #3b8070;
+                border-radius: 100%;
+                -webkit-animation: sk-circleBounceDelay 1.2s infinite ease-in-out both;
+                animation: sk-circleBounceDelay 1.2s infinite ease-in-out both
+            }
+
+            .sk-circle .sk-circle2 {
+                -webkit-transform: rotate(30deg);
+                -ms-transform: rotate(30deg);
+                transform: rotate(30deg)
+            }
+
+            .sk-circle .sk-circle3 {
+                -webkit-transform: rotate(60deg);
+                -ms-transform: rotate(60deg);
+                transform: rotate(60deg)
+            }
+
+            .sk-circle .sk-circle4 {
+                -webkit-transform: rotate(90deg);
+                -ms-transform: rotate(90deg);
+                transform: rotate(90deg)
+            }
+
+            .sk-circle .sk-circle5 {
+                -webkit-transform: rotate(120deg);
+                -ms-transform: rotate(120deg);
+                transform: rotate(120deg)
+            }
+
+            .sk-circle .sk-circle6 {
+                -webkit-transform: rotate(150deg);
+                -ms-transform: rotate(150deg);
+                transform: rotate(150deg)
+            }
+
+            .sk-circle .sk-circle7 {
+                -webkit-transform: rotate(180deg);
+                -ms-transform: rotate(180deg);
+                transform: rotate(180deg)
+            }
+
+            .sk-circle .sk-circle8 {
+                -webkit-transform: rotate(210deg);
+                -ms-transform: rotate(210deg);
+                transform: rotate(210deg)
+            }
+
+            .sk-circle .sk-circle9 {
+                -webkit-transform: rotate(240deg);
+                -ms-transform: rotate(240deg);
+                transform: rotate(240deg)
+            }
+
+            .sk-circle .sk-circle10 {
+                -webkit-transform: rotate(270deg);
+                -ms-transform: rotate(270deg);
+                transform: rotate(270deg)
+            }
+
+            .sk-circle .sk-circle11 {
+                -webkit-transform: rotate(300deg);
+                -ms-transform: rotate(300deg);
+                transform: rotate(300deg)
+            }
+
+            .sk-circle .sk-circle12 {
+                -webkit-transform: rotate(330deg);
+                -ms-transform: rotate(330deg);
+                transform: rotate(330deg)
+            }
+
+            .sk-circle .sk-circle2:before {
+                -webkit-animation-delay: -1.1s;
+                animation-delay: -1.1s
+            }
+
+            .sk-circle .sk-circle3:before {
+                -webkit-animation-delay: -1s;
+                animation-delay: -1s
+            }
+
+            .sk-circle .sk-circle4:before {
+                -webkit-animation-delay: -.9s;
+                animation-delay: -.9s
+            }
+
+            .sk-circle .sk-circle5:before {
+                -webkit-animation-delay: -.8s;
+                animation-delay: -.8s
+            }
+
+            .sk-circle .sk-circle6:before {
+                -webkit-animation-delay: -.7s;
+                animation-delay: -.7s
+            }
+
+            .sk-circle .sk-circle7:before {
+                -webkit-animation-delay: -.6s;
+                animation-delay: -.6s
+            }
+
+            .sk-circle .sk-circle8:before {
+                -webkit-animation-delay: -.5s;
+                animation-delay: -.5s
+            }
+
+            .sk-circle .sk-circle9:before {
+                -webkit-animation-delay: -.4s;
+                animation-delay: -.4s
+            }
+
+            .sk-circle .sk-circle10:before {
+                -webkit-animation-delay: -.3s;
+                animation-delay: -.3s
+            }
+
+            .sk-circle .sk-circle11:before {
+                -webkit-animation-delay: -.2s;
+                animation-delay: -.2s
+            }
+
+            .sk-circle .sk-circle12:before {
+                -webkit-animation-delay: -.1s;
+                animation-delay: -.1s
+            }
+
+            @-webkit-keyframes sk-circleBounceDelay {
+
+                0%,
+                100%,
+                80% {
+                    -webkit-transform: scale(0);
+                    transform: scale(0)
                 }
 
-                20% {
-                    visibility: visible;
-                    opacity: 0
-                }
-
-                100% {
-                    visibility: visible;
-                    opacity: 1
+                40% {
+                    -webkit-transform: scale(1);
+                    transform: scale(1)
                 }
             }
 
-            @-webkit-keyframes nuxtLoadingIn {
-                0% {
-                    visibility: hidden;
-                    opacity: 0
+            @keyframes sk-circleBounceDelay {
+
+                0%,
+                100%,
+                80% {
+                    -webkit-transform: scale(0);
+                    transform: scale(0)
                 }
 
-                20% {
-                    visibility: visible;
-                    opacity: 0
-                }
-
-                100% {
-                    visibility: visible;
-                    opacity: 1
-                }
-            }
-
-            #nuxt-loading>div,
-            #nuxt-loading>div:after {
-                border-radius: 50%;
-                width: 5rem;
-                height: 5rem
-            }
-
-            #nuxt-loading>div {
-                font-size: 10px;
-                position: relative;
-                text-indent: -9999em;
-                border: .5rem solid #f5f5f5;
-                border-left: .5rem solid #000;
-                -webkit-transform: translateZ(0);
-                -ms-transform: translateZ(0);
-                transform: translateZ(0);
-                -webkit-animation: nuxtLoading 1.1s infinite linear;
-                animation: nuxtLoading 1.1s infinite linear
-            }
-
-            #nuxt-loading.error>div {
-                border-left: .5rem solid #ff4500;
-                animation-duration: 5s
-            }
-
-            @-webkit-keyframes nuxtLoading {
-                0% {
-                    -webkit-transform: rotate(0);
-                    transform: rotate(0)
-                }
-
-                100% {
-                    -webkit-transform: rotate(360deg);
-                    transform: rotate(360deg)
-                }
-            }
-
-            @keyframes nuxtLoading {
-                0% {
-                    -webkit-transform: rotate(0);
-                    transform: rotate(0)
-                }
-
-                100% {
-                    -webkit-transform: rotate(360deg);
-                    transform: rotate(360deg)
+                40% {
+                    -webkit-transform: scale(1);
+                    transform: scale(1)
                 }
             }
         </style>
-        <script>
-            window.addEventListener("error", function() {
-                var e = document.getElementById("nuxt-loading");
-                e && (e.className += " error")
-            })
-        </script>
-        <div id="nuxt-loading" aria-live="polite" role="status">
-            <div>Loading...</div>
+        <div class="sk-circle">
+            <div class="sk-circle1 sk-child"></div>
+            <div class="sk-circle2 sk-child"></div>
+            <div class="sk-circle3 sk-child"></div>
+            <div class="sk-circle4 sk-child"></div>
+            <div class="sk-circle5 sk-child"></div>
+            <div class="sk-circle6 sk-child"></div>
+            <div class="sk-circle7 sk-child"></div>
+            <div class="sk-circle8 sk-child"></div>
+            <div class="sk-circle9 sk-child"></div>
+            <div class="sk-circle10 sk-child"></div>
+            <div class="sk-circle11 sk-child"></div>
+            <div class="sk-circle12 sk-child"></div>
         </div>
     </div>
     <script>
@@ -139,10 +232,10 @@
             }
         }
     </script>
-    <script src="/_nuxt/0b5f6f3.js"></script>
-    <script src="/_nuxt/3d380bf.js"></script>
-    <script src="/_nuxt/acd3085.js"></script>
-    <script src="/_nuxt/4f0ec5f.js"></script>
+    <script src="/_nuxt/f52530e.js"></script>
+    <script src="/_nuxt/47db05d.js"></script>
+    <script src="/_nuxt/af631b6.js"></script>
+    <script src="/_nuxt/58c456c.js"></script>
 </body>
 
 </html>
