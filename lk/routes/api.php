3b196100011b0d5dc97d488dae51f6d92ca49798
monsidev <monsidev@gmail.com>
2021-06-22 17:54:38 +0300

    dasd

--- lk/routes/api.php
+++ lk/routes/api.php
@@ -6,52 +6,6 @@ use Illuminate\Support\Facades\Route;
 /**
  * Здесь Get Запросы
  */
-
-use App\Helpers\Dreamkas\Api as Dreamkas;
-use App\Helpers\Dreamkas\CustomerAttributes;
-use App\Helpers\Dreamkas\exceptions\ValidationException;
-use App\Helpers\Dreamkas\Payment as DreamkasPayment;
-use App\Helpers\Dreamkas\Position;
-use App\Helpers\Dreamkas\Receipt;
-use App\Helpers\Dreamkas\TaxMode;
-use GuzzleHttp\Exception\ClientException;
-
-Route::get('/receipt/test', function () {
-    $dreamkasApi = new Dreamkas('2aabc4ab-4844-4378-ab37-9ee60bde7064', 129174, Dreamkas::MODE_PRODUCTION);
-    $receipt = new Receipt();
-    $receipt->taxMode = TaxMode::MODE_PATENT;
-    $receipt->positions[] = new Position([
-        'name' => 'лк - тест',
-        'quantity' => 1,
-        'price' => 100, // цена в копейках за 1 шт. или 1 грамм
-    ]);
-    $receipt->payments[] = new DreamkasPayment([
-        'sum' => 100, // стоимость оплаты по чеку
-    ]);
-    $receipt->attributes = new CustomerAttributes([
-        'email' => 'monsidev@gmail.com', // почта покупателя
-        'phone' => '+7(938) 305-07-24', // телефон покупателя
-    ]);
-    // $receipt->calculateSum();
-    // // А можно завалидировать чек
-    // $receipt->validate();
-    $response = [];
-    try {
-        $response = $dreamkasApi->postReceipt($receipt);
-    } catch (ValidationException $e) {
-        return response()->json([
-            'errors_message' => $e->getMessage()
-        ], 400);
-    } catch (ClientException $e) {
-        return response($e->getResponse()->getBody());
-        // Это исключение кидается, когда при передачи чека в Дрикас произошла ошибка. Лучше отправить чек ещё раз
-        // Если будут дубли - потом отменяйте через $receipt->type = Receipt::TYPE_REFUND;
-    }
-    return response()->json([
-        'response' => $response,
-    ]);
-});
-
 //EMPTY
 Route::get('/directory/categories', function () {
     return \DB::table('categories')->get();
