c4bd5a05cbc400a5f8517541c5a7e89de7dc7b20
monsidev <monsidev@gmail.com>
2021-07-01 13:38:12 +0300

    dasd

--- lk/app/Console/Commands/BackupService.php
+++ lk/app/Console/Commands/BackupService.php
@@ -55,7 +55,7 @@ class BackupService extends Command
 
         try {
             // SelectelApi::storeFile('LM.RESERVE', storage_path("app/private/database/" . date("d.m.Y") . '.sql'), date("d.m.Y") . '.sql');
-            Storage::disk('S3')->put(date("d.m.Y") . 'sql', Storage::disk('private')->get('database/' . date("d.m.Y") . '.sql'));
+            Storage::disk('s3')->put(date("d.m.Y") . 'sql', Storage::disk('private')->get('database/' . date("d.m.Y") . '.sql'));
         } catch(Exception $e) {
             Log::error($e->getMessage());
         }
